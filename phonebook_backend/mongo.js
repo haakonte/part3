4cd82ad8b03737d18ebcd91073e2fa5c2d3de617
Håkon Telje <haakon.telje@gmail.com>
2020-12-14 16:15:54 +0100

    finished 3.12

---
+++ phonebook_backend/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack_backend:${password}@cluster0.6ihqu.mongodb.net/person-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (!name || !number) {
+  console.log("Phonebook:")
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const person = new Person({
+    name,
+    number
+  })
+  
+  person.save().then(result => {
+    console.log(`Added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
+
+
+
+
