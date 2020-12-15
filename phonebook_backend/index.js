68b9dd9738fe294a6180585f0fde6ffc3660c2b1
Håkon Telje <haakon.telje@gmail.com>
2020-12-15 18:23:35 +0100

    completed 3.20

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -63,21 +63,21 @@ app.get('/api/persons/:id', (request, response, next) => {
 /**
  * CRUD method for posting a new person
  */
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  
-  if (body.name === undefined) {
-    return response.status(400).json({error: "Name missing"})
-  }
 
   const person = new Person({
     name: body.name,
     number: body.number
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson.toJSON())
-  })
+  person
+    .save()
+    .then(savedPerson => savedPerson.toJSON())
+    .then(savedAndFormattedPerson => {
+      response.json(savedAndFormattedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -116,9 +116,12 @@ app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
+  
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
+  }
   next(error)
 }
 
